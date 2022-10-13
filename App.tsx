442b79c70014acc14b412b1601df191e66f6ae6c
pyuun <84262485+pyuun@users.noreply.github.com>
2022-10-13 20:41:07 -0300

    Test navi

--- App.tsx
+++ App.tsx
@@ -1,43 +1,38 @@
-import HomeScreen from './src/screen/HomeScreen';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-import Registrar from './src/screen/Registrar';
-import MisCompras from './src/screen/MisCompras';
+import HomeScreen from "./src/screen/HomeScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import Registrar from "./src/screen/Registrar";
+import MisCompras from "./src/screen/MisCompras";
 
 // Stacks
 const Stack = createNativeStackNavigator();
 
 export type TabNavigatorParamList = {
-  Home: undefined,
-  MisCompras: undefined,
-  Registrar: undefined
-}
+  Home: undefined;
+  MisCompras: undefined;
+  Registrar: undefined;
+};
 // Tabbis
 const Tab = createMaterialBottomTabNavigator<TabNavigatorParamList>();
 
+function HomeTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="MisCompras" component={MisCompras} />
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
   return (
-
     <NavigationContainer>
-      {/* <Stack.Navigator>
-        <Stack.Screen name="Home" component= {HomeScreen}/>
-      </Stack.Navigator> */}
-      {/* Como pregunta mas que nada, porque tengo que reiniciar el maldito servidor este aveces si y yaveces no lmao  */}
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        {/* TODO: Aca tipo mi idea es poner "mis compras"(pa mostrar las compras y su estado) y llevarlo a registrar de no estar registrado? no se si siquiera podes entrar aca sin estarlo no se, */}
-        {/* <Tab.Screen name="registrar" component={Registrar}/> */}
+      <Stack.Navigator>
+        <Stack.Screen name="HomeTabs" component={HomeTabs} />
 
-        {/* Por ahora queda asi */}
-        <Tab.Screen name="MisCompras" component={MisCompras}/>
-        <Tab.Screen name="Registrar" component={Registrar}/>
-      </Tab.Navigator>
-      
-
-      
+        <Stack.Screen name="Registrar" component={Registrar} />
+      </Stack.Navigator>
     </NavigationContainer>
-
   );
 }
