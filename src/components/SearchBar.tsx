88103059f5fcf38c30c9772846996584132952f5
pyuun <84262485+pyuun@users.noreply.github.com>
2022-10-12 21:32:00 -0300

    Sin papers
    No me conformo el papers.

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,15 +1,52 @@
-import { SafeAreaView, Text } from 'react-native'
-import React from 'react'
-import {useNavigation} from '@react-navigation/native'
-// TODO: Consulta, cosas que tengo como "use navigation" y "safeareaveiw" tengo que copipastearlas o las puedo poner en otro lado para evitar?
+import { SafeAreaView, Text , StyleSheet} from 'react-native'
+import React, {useLayoutEffect} from 'react'
 
+import {useNavigation} from '@react-navigation/native'
+import { TextInput, IconButton} from '@react-native-material/core';
+import Icon from "@expo/vector-icons/MaterialCommunityIcons";
+
+
+const styles = StyleSheet.create(
+  {
+    // Copy de el frontend
+    searchBar: {
+      height: 32,
+      marginLeft: "auto",
+      marginRight: "auto",
+      textAlign: "center"
+    },
+  }
+)
 
 const SearchBar = () => {
-  return (
-    <SafeAreaView>
-      <Text>SearchBar</Text>
-    </SafeAreaView>
-  )
+  // const [searchQuery, setSearchQuery] = React.useState('')
+  // const onChangeSearch = (query) => setSearchQuery(query)
+  const navi = useNavigation();
+  useLayoutEffect(()=> navi.setOptions({headerShown:false}))
+
+    return (
+      
+      <SafeAreaView>
+        {/* Heroicons y react-nativesvg pa esto  */}
+        <TextInput
+          // Intente stylearlo asi nomas y quedo medio raro??
+          // style = {styles.searchBar }
+          placeholder='Buscar'
+          // inlineImageLeft='search_icon'
+          trailing={props => (
+          
+              <IconButton
+                icon={props => <Icon name="magnify" {...props} />}
+                {...props}
+              />
+          )}
+
+        />
+       </SafeAreaView>
+    )
+   
+    
+
 }
 
 export default SearchBar
--- src/screen/HomeScreen.tsx
+++ src/screen/HomeScreen.tsx
@@ -1,31 +1,40 @@
-import { View, Text, SafeAreaView } from 'react-native'
-import React, { useLayoutEffect } from 'react'
-import { AppBar } from '@react-native-material/core'
-import {useNavigation } from "@react-navigation/native"
-import SearchBar from '../components/SearchBar'
-
-
-
-
+import { View, Text, SafeAreaView } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { AppBar, HStack, IconButton } from "@react-native-material/core";
+import { useNavigation } from "@react-navigation/native";
+import SearchBar from "../components/SearchBar";
+import Icon from "@expo/vector-icons/MaterialCommunityIcons";
 
 const HomeScreen = () => {
   // Hook para quitar el auto-header de navi
   const navi = useNavigation();
 
-
-useLayoutEffect(() => navi.setOptions( {
-  // headerTitle: "a"
-  headerShown: false
-}), [] )
-
+  useLayoutEffect(
+    () =>
+      navi.setOptions({
+        // headerTitle: "a"
+        headerShown: false,
+      }),
+    []
+  );
 
   return (
     // SafeAreaView creo que deberia permitir que los cell digan que tanto bajar la header por ejemplo (no creo que ufnque en el que tengo yo)
     <SafeAreaView>
-      <AppBar title="Home" />
-      <SearchBar/>
-    </SafeAreaView>
-  )
-}
+      <AppBar
+      title="Title"
 
-export default HomeScreen
+        leading={(props) => (
+          <IconButton
+            icon={(props) => <Icon name="menu-down" {...props} />}
+            {...props}
+          />
+        )}
+      />
+
+      <SearchBar />
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
