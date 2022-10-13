6e56d4c362daf8cec4ca57fcf39a305640722015
Mauro Curbelo <maurohcurbelo@gmail.com>
2022-10-13 09:52:06 -0300

    Arreglar problemas con el navigator

--- App.tsx
+++ App.tsx
@@ -8,8 +8,13 @@ import MisCompras from './src/screen/MisCompras';
 // Stacks
 const Stack = createNativeStackNavigator();
 
+export type TabNavigatorParamList = {
+  Home: undefined,
+  MisCompras: undefined,
+  Registrar: undefined
+}
 // Tabbis
-const Tab = createMaterialBottomTabNavigator();
+const Tab = createMaterialBottomTabNavigator<TabNavigatorParamList>();
 
 
 export default function App() {
@@ -21,14 +26,15 @@ export default function App() {
       </Stack.Navigator> */}
       {/* Como pregunta mas que nada, porque tengo que reiniciar el maldito servidor este aveces si y yaveces no lmao  */}
       <Tab.Navigator>
-        <Tab.Screen name="home" component={HomeScreen} />
+        <Tab.Screen name="Home" component={HomeScreen} />
         {/* TODO: Aca tipo mi idea es poner "mis compras"(pa mostrar las compras y su estado) y llevarlo a registrar de no estar registrado? no se si siquiera podes entrar aca sin estarlo no se, */}
         {/* <Tab.Screen name="registrar" component={Registrar}/> */}
 
         {/* Por ahora queda asi */}
-        <Tab.Screen name="misCompras" component={MisCompras}/>
+        <Tab.Screen name="MisCompras" component={MisCompras}/>
+        <Tab.Screen name="Registrar" component={Registrar}/>
       </Tab.Navigator>
-      <Tab.Screen name="registrarse" component={Registrar}/>
+      
 
       
     </NavigationContainer>
--- src/screen/MisCompras.tsx
+++ src/screen/MisCompras.tsx
@@ -4,17 +4,18 @@ import React, {useLayoutEffect} from 'react'
 import {useNavigation} from '@react-navigation/native'
 import { Stack, Button } from "@react-native-material/core";
  import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { MaterialBottomTabNavigationProp, MaterialBottomTabScreenProps } from '@react-navigation/material-bottom-tabs';
+import { TabNavigatorParamList } from '../../App';
 
 
-const MisCompras = ({navigation}) => {
-
+type MisComprasProps = MaterialBottomTabScreenProps<TabNavigatorParamList, "MisCompras">
+const MisCompras = ({route, navigation}: MisComprasProps) => {
 
   return (
 
     <Stack fill center spacing={4}>     
       <Button title="REGISTRE-SE"
-      onPress={() => navigation.navigate('registrarse')}
+      onPress={() => navigation.navigate('Registrar')}
 
        />
 
