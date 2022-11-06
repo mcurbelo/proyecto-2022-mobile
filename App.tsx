087ef9fa9620d880ed7484e7be89d3838bbb22ec
pyuun <84262485+pyuun@users.noreply.github.com>
2022-11-06 18:16:28 -0300

    Don't ask

--- App.tsx
+++
@@ -1,9 +0,0 @@
-import { SafeAreaView } from "react-native-safe-area-context";
-import Navigator from "./src/navigator/Navigator";
-import { StyleSheet } from "react-native";
-const styles = StyleSheet.create({ root: { backgroundColor: "#F0FBFC" } });
-export default function App() {
-  return (
-      <Navigator />
-  );
-}
